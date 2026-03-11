59661f7e3d78ed173f4755d52bd3a54da8af54c4
juniorbehruz08 <farhodovbehruz2@gmail.com>
2026-03-11 17:37:00 +0500

    for privacy 2

--- project/settings.py
+++ project/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = "/var/www/static/"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
